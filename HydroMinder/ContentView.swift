e235954df321343481b7acbd536ea245d7aa16d4
park <rudeo7701@gmail.com>
2024-04-15 23:00:20 +0900

    앱 언어 한글로 수정
    * 기본값 한글
    * 앱아이콘 추가
    * coreData Item Atrribute uuid 추가
    * noti 추가, 삭제 구현
    * 노티 문구 추가

--- HydroMinder/ContentView.swift
+++ HydroMinder/ContentView.swift
@@ -25,7 +25,6 @@ class NotificationDelegate: NSObject, UNUserNotificationCenterDelegate {
     }
 
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        // 앱이 포그라운드에서 실행 중일 때도 알림이 표시되도록 설정
         completionHandler([.list, .badge, .sound])
     }
 }
@@ -34,32 +33,99 @@ class NotificationDelegate: NSObject, UNUserNotificationCenterDelegate {
 struct Reminder : View{
     // 값이 바껴야하기 때문에 옵저버로 지정
     @ObservedObject var item : Item
-    
     @Environment(\.managedObjectContext) private var viewContext
+    
+    let notificationMessages = [
+        "물 마실 시간입니다! 건강을 위해 잠시 일어나세요.",
+        "잠깐! 수분 보충의 시간이에요. 물 한 잔 어때요?",
+        "하루에 물 8잔은 건강의 비결입니다. 지금 마셔보세요!",
+        "물 한 잔으로 에너지를 충전하세요! 당신의 몸이 감사할 거에요.",
+        "잊지 마세요, 물은 최고의 보약입니다. 지금 한 잔 하세요.",
+        "물 마시는 시간! 몸을 리프레시하고 에너지를 충전하세요.",
+        "물을 마시면 피로가 풀립니다. 잠시 휴식을 취하며 몸을 재충전하세요.",
+        "물 한 잔으로 두뇌에 활력을 주세요. 집중력이 필요할 때 바로 그때입니다.",
+        "매시간 물 한잔은 건강한 생활의 시작입니다. 잊지 마세요!",
+        "물은 자연의 보약입니다. 지금 바로 건강을 챙기세요.",
+        "건강을 위해, 오늘 물 충분히 드셨나요? 지금이 마실 때입니다!"
+    ]
+    
+    private func addNotification(datetime: Date, uuid: UUID)
+    {
+        
+        let content = UNMutableNotificationContent()
+        content.title = "HydroMinder"
+        content.body = notificationMessages.randomElement()!
+        content.sound = UNNotificationSound.default
+
+        let hh = itemFormatterHH.string(from: datetime)
+        let mm = itemFormatterMM.string(from: datetime)
+
+        // 알림을 보낼 날짜와 시간 설정
+        var dateComponents = DateComponents()
+        dateComponents.hour = Int(hh)
+        dateComponents.minute = Int(mm)
+        
+
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+
+        // 알림 요청 생성
+        let request = UNNotificationRequest(identifier: uuid.uuidString, content: content, trigger: trigger)
+
+        // 알림 센터에 추가
+        center.add(request) { error in
+            if let error = error {
+                print("알림 스케줄 실패: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    func removeNotification(uuid: UUID) {
+        let center = UNUserNotificationCenter.current()
+        center.removePendingNotificationRequests(withIdentifiers: [uuid.uuidString])
+    }
+    
+    func removeAllNotifications() {
+        let center = UNUserNotificationCenter.current()
+        center.removeAllPendingNotificationRequests()
+    }
     
     var body: some View{
         HStack{
-            HStack{
-                Text("\(item.timestamp!, formatter: itemFormatterAm)")
+            if let timestamp = item.timestamp {
+                HStack{
+                    Text("\(timestamp, formatter: itemFormatterAm)")
+                        .offset(x: -10, y: 5)
+                    
+                    
+                    Text("\(timestamp, formatter: itemFormatter)")
+                        .font(.system(size: 30))
+                        .offset(x: -15)
+                }
+            } else {
+                Text("No timestamp")
                     .offset(x: -10, y: 5)
-                
-                
-                Text("\(item.timestamp!, formatter: itemFormatter)")
-                    .font(.system(size: 30))
-                    .offset(x: -15)
             }
             Toggle("", isOn: Binding<Bool>(
                 get: { self.item.isChecked },
                 set: { newValue in
                     self.item.isChecked = newValue
                     // CoreData 컨텍스트에 변경 사항을 저장
+                    
+                    if(newValue)
+                    {
+                        addNotification(datetime: item.timestamp!,uuid: item.uuid!)
+                    }else
+                    {
+                        removeNotification(uuid: item.uuid!)
+                    }
+                    // Date 객체를 원하는 형식의 문자열로 변환
+
                     try? self.viewContext.save()
                 }
             ))
         }
     }
 }
-
 
 struct ContentView: View {
     
@@ -70,66 +136,36 @@ struct ContentView: View {
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
         animation: .default)
+    
     private var items: FetchedResults<Item>
-    
-    
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-            newItem.isChecked = false
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
     
     private func deleteItems(offsets: IndexSet) {
         withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
+            offsets.forEach { index in
+                let item = items[index]
+                
+                removeNotification(uuid: item.uuid!)
+                
+                viewContext.delete(item)
+            }
 
             do {
                 try viewContext.save()
             } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
                 let nsError = error as NSError
                 fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
         }
     }
     
-    private func addNotification()
-    {
-        let content = UNMutableNotificationContent()
-        content.title = "물 마시자"
-        content.body = "물 마실 시간입니다."
-        content.sound = UNNotificationSound.default
-
-        // 알림을 보낼 날짜와 시간 설정
-        var dateComponents = DateComponents()
-        dateComponents.hour = 19    // 24시간 표기법 사용, 오후 7시
-        dateComponents.minute = 7  // 10분
-        
-        // 5분 후 알림 설정
-        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
-
-        // 알림 요청 생성
-        let request = UNNotificationRequest(identifier: "UniqueIdentifier", content: content, trigger: trigger)
-
-        // 알림 센터에 추가
-        center.add(request) { error in
-            if let error = error {
-                print("알림 스케줄 실패: \(error.localizedDescription)")
-            }
-        }
+    func removeNotification(uuid: UUID) {
+        let center = UNUserNotificationCenter.current()
+        center.removePendingNotificationRequests(withIdentifiers: [uuid.uuidString])
+    }
+    
+    func removeAllNotifications() {
+        let center = UNUserNotificationCenter.current()
+        center.removeAllPendingNotificationRequests()
     }
     
     init(showingSheet: Bool = false) {
@@ -143,11 +179,6 @@ struct ContentView: View {
                 print("알림 거부됨")
             }
         }
-        
-
-        
-        addNotification()
-
     }
     
     var body: some View {
@@ -157,6 +188,7 @@ struct ContentView: View {
                 ForEach(items) { item in
                     Reminder(item: item)
                 }
+                .onDelete(perform: deleteItems)
             }
                 .navigationBarTitle("물마시기")
                 .navigationBarItems(trailing: Button(action: {
@@ -181,29 +213,36 @@ struct DetailView: View {
         animation: .default)
     private var items: FetchedResults<Item>
     
-    private func addItem() {
+    private func addItem(datetime: Date) {
         withAnimation {
             let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
+            newItem.timestamp = datetime
             newItem.isChecked = false
+            newItem.uuid = UUID()
 
             do {
                 try viewContext.save()
             } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
                 let nsError = error as NSError
                 fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
         }
     }
+    @State private var date = Date()
     
     var body:some View {
         NavigationView {
-            HStack{
-                Text("시간")
 
+            HStack {
+
+                DatePicker("  시간", selection: $date, displayedComponents: [.hourAndMinute])
+                    .datePickerStyle(GraphicalDatePickerStyle())
+                    .foregroundColor(.black)
+                    .contentMargins(-5)
+
+                    .environment(\.colorScheme, .dark) // <- This modifier
             }
+            .toggleStyle(.button) // 버튼 스타일 적용
                 .navigationBarTitle("알림 추가", displayMode: .inline)
                 .navigationBarItems(
                     leading: Button("취소") {
@@ -211,12 +250,14 @@ struct DetailView: View {
                 },
                     trailing: Button("저장") {
                         // 오른쪽 버튼 액션
-                        addItem()
+                        addItem(datetime: date)
+                        presentationMode.wrappedValue.dismiss()
                     }
                 )
         }
     }
 }
+
 
 private let itemFormatter: DateFormatter = {
     let formatter = DateFormatter()
@@ -230,6 +271,21 @@ private let itemFormatterAm: DateFormatter = {
     formatter.locale = Locale(identifier: "ko_KR")
     return formatter
 }()
+
+private let itemFormatterHH: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "HH"
+    formatter.locale = Locale(identifier: "ko_KR")
+    return formatter
+}()
+
+private let itemFormatterMM: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "mm"
+    formatter.locale = Locale(identifier: "ko_KR")
+    return formatter
+}()
+
 
 #Preview {
     ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
